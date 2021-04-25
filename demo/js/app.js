a7f504f0ec05a442c459fa6214b3b6cf4b24559f
Mahmoud-Barghouthi <mah.barghouthi47@gmail.com>
2021-04-25 14:07:15 +0300

    edit

---
+++ demo/js/app.js
@@ -0,0 +1,42 @@
+/*
+this is a comment in the js file 
+
+var variable
+
+let 
+const
+
+*/
+
+// let baraah = 'baraah';
+// // it will give me the authority to declare the variable name only one time
+
+// let baraah = 'Ola';
+// console.log(baraah);
+
+// I cannot use it before initialize it 
+// let car;
+// car = 'BMW';
+// console.log(car);
+
+
+// const I can declare it and use it only one time
+// const url = 'https://github.com/';
+// console.log(url);
+
+// const url = 'baraa\'h';
+
+
+let userName = prompt('What\'s your name Soldier');
+console.log(userName);
+
+let loadout = prompt('What\'s your loadout');
+console.log(loadout);
+
+let map = prompt('What map do you like most?\nVerdansk or Rebirth ');
+console.log(map);
+
+let favChar = prompt('Please name your favorite Character');
+console.log(favChar);
+
+alert('Lieutenant ' + userName + ' your favorite loadout is ' + loadout + ' your favorite map is  ' + map + ' and your favorite Character is ' + favChar);
